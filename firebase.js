8e6fdf592822ccf19d295d820e82fb8e3cb8fa0d
Rick <ricardolau72@gmail.com>
2024-06-26 19:40:02 -0500

    Commit 6

--- firebase.js
+++ firebase.js
@@ -3,7 +3,13 @@ const admin = require('firebase-admin');
 const serviceAccount = require('./serviceAccount.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  // credential: admin.credential.cert(serviceAccount)
+  apiKey: "AIzaSyA8az79YztNdpDVnypxSvVXlsSj80xk0p8",
+  authDomain: "yosyrick-business.firebaseapp.com",
+  projectId: "yosyrick-business",
+  storageBucket: "yosyrick-business.appspot.com",
+  messagingSenderId: "487688553467",
+  appId: "1:487688553467:web:84ca0fd2b481a49bc4c15e"
 });
 
 const db = admin.firestore();
